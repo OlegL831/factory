32a6ea35282da25cb1f6a5396f7ded0c71896416
Olege <example@gmail.comm>
2022-09-22 18:19:23 +0300

    section header+carousel

---
+++ src/js/myscript.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 4000,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        prevArrow: false,
+        nextArrow: false,
+        dots: true,
+        dotsClass: 'slick-dots',
+    })
+});
