82195589b0a9ea3dfb874178ad7ccd00a8bfd0a7
Oleg <example@gmail.comm>
2022-09-24 18:10:12 +0300

    section production

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,11 +1,20 @@
 $(document).ready(function(){
     $('.carousel_inner').slick({
         speed: 4000,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 3000,
         prevArrow: false,
         nextArrow: false,
         dots: true,
         dotsClass: 'slick-dots',
+    })
+
+    $('.production_carousel').slick({
+        arrows: true,
+        speed: 2000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 6,
+        slidesToScroll: 2,
     })
 });
